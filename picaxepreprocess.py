dae008c29967d8c030b498788b4c55a13704c863
Patrick Leiser <patrick27leiser@yahoo.com>
2023-03-30 01:40:24 -0700

    adjust whitespace stripping to resolve issue with spaces between opening parenthesis and argument

--- picaxepreprocess.py
+++ picaxepreprocess.py
@@ -398,8 +398,7 @@ Called from line {} in '{}'""".format(curpath, curfilename, called_from_line, ca
                         with open (outputfilename, 'a') as output_file:
                             output_file.write("'PARSED MACRO "+macroname)
                         macrocontents=workingline.split("(", maxsplit=1)[1].split(")", maxsplit=1)[0].rstrip()
-
-                        macros[macroname] = {i + 1: m.rstrip() for i, m in enumerate(macrocontents.split(','))}
+                        macros[macroname] = {i + 1: m.strip() for i, m in enumerate(macrocontents.split(','))}
                         macros[macroname][0]="'--START OF MACRO: "+macroname+"\n"
                         preprocessor_info("finished parsing macro contents")
                         preprocessor_info(macros[macroname])
